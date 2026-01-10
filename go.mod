60546187769c19ca68d4b809649a3f7de3dba3b7
YeiyoNathnael <138720671+YeiyoNathnael@users.noreply.github.com>
2026-01-10 04:56:07 +0400

    fix: fixed file structure,removed an already revoked secret, and the code using it

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
-module github.com/YeiyoNathnael/ethchess-bot-tewdros
+module commands
 
 go 1.25.5
 
-require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.33 // indirect
+require github.com/PaulSonOfLars/gotgbot/v2 v2.0.0-rc.33
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,41 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	//"os"
-	"time"
-
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
-	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers/filters/message"
+	"github.com/joho/godotenv"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-// This bot is as basic as it gets - it simply repeats everything you say.
-// The main_test.go file contains example code to demonstrate how to implement the gotgbot.BotClient interface for it to be used in tests.
 func main() {
 	// Get token from the environment variable
-	token := "7720642643:AAGwKlD1srFmjzf3-Q6S2qkDRhNIEcuzKq0" //os.Getenv("TOKEN")
+	err := godotenv.Load()
+	token := os.Getenv("TOKEN")
 	if token == "" {
 		panic("TOKEN environment variable is empty")
 	}
 
 	// Create bot from environment value.
-	b, err := gotgbot.NewBot(token, &gotgbot.BotOpts{
-		BotClient: &gotgbot.BaseBotClient{
-			Client: http.Client{},
-			DefaultRequestOpts: &gotgbot.RequestOpts{
-				Timeout: gotgbot.DefaultTimeout, // Customise the default request timeout here
-				APIURL:  gotgbot.DefaultAPIURL,  // As well as the Default API URL here (in case of using local bot API servers)
-			},
-		},
-	})
+	b, err := gotgbot.NewBot(token, nil)
 	if err != nil {
 		panic("failed to create new bot: " + err.Error())
 	}
-
 	// Create updater and dispatcher.
 	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
 		// If an error is returned by a handler, log it and continue going.
@@ -45,12 +39,16 @@ func main() {
 		},
 		MaxRoutines: ext.DefaultMaxRoutines,
 	})
-	updater := ext.NewUpdater(dispatcher, nil)
+	updater := ext.NewUpdater(dispatcher, nil) //&ext.UpdaterOpts{ErrorLog: logger})
 
-	// Add echo handler to reply to all text messages.
-	dispatcher.AddHandler(handlers.NewMessage(message.Text, echo))
+	// /start command to introduce the bot
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+	dispatcher.AddHandler(handlers.NewCommand("blitz", blitz))
+	dispatcher.AddHandler(handlers.NewCommand("blitzr", blitzr))
+	dispatcher.AddHandler(handlers.NewCommand("bullet", bullet))
+	dispatcher.AddHandler(handlers.NewCommand("bulletr", bulletr))
+	dispatcher.AddHandler(handlers.NewCommand("open", open))
 
-	// Start receiving updates.
 	err = updater.StartPolling(b, &ext.PollingOpts{
 		DropPendingUpdates: true,
 		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
@@ -63,17 +61,151 @@ func main() {
 	if err != nil {
 		panic("failed to start polling: " + err.Error())
 	}
-	log.Printf("%s has been started...\n", b.User.Username)
 
 	// Idle, to keep updates coming in, and avoid bot stopping.
 	updater.Idle()
 }
 
-// echo replies to a messages with its own contents.
-func echo(b *gotgbot.Bot, ctx *ext.Context) error {
-	_, err := ctx.EffectiveMessage.Reply(b, ctx.EffectiveMessage.Text, nil)
+func openChallenge(b *gotgbot.Bot, ctx *ext.Context, clockLimit string, clockIncrement string, duelName string, rated bool) error {
+
+	urlL := "https://lichess.org/api/challenge/open"
+
+	postData := url.Values{}
+	postData.Set("rated", strconv.FormatBool(rated))
+	postData.Set("clock.limit", clockLimit)
+	postData.Set("clock.increment", clockIncrement)
+	postData.Set("days", "1")
+	postData.Set("variant", "standard")
+	postData.Set("fen", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+	postData.Set("name", duelName)
+
+	req, _ := http.NewRequest("POST", urlL, strings.NewReader(postData.Encode()))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+
+	var LichessChallengeResponse struct {
+		Url string `json:"url"`
+	}
+
+	if err := json.Unmarshal(body, &LichessChallengeResponse); err != nil {
+		return fmt.Errorf("parsing failed: %w", err)
+	}
+
+	link := LichessChallengeResponse.Url
+
+	_, err := ctx.EffectiveMessage.Reply(b, link, &gotgbot.SendMessageOpts{
+		ParseMode: "HTML",
+	},
+	)
 	if err != nil {
-		return fmt.Errorf("failed to echo message: %w", err)
+		return fmt.Errorf("failed to send source: %w", err)
+	}
+
+	return nil
+
+}
+
+func blitz(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	openChallenge(b, ctx, "180", "2", "Grand Blitz Duel", false)
+
+	return nil
+
+}
+
+func blitzr(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	openChallenge(b, ctx, "180", "2", "Grand Blitz Duel", true)
+
+	return nil
+
+}
+
+func bullet(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	openChallenge(b, ctx, "60", "0", "Grand Blitz Duel", false)
+
+	return nil
+
+}
+
+func bulletr(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	openChallenge(b, ctx, "60", "0", "Grand Blitz Duel", true)
+
+	return nil
+
+}
+
+func open(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	args := ctx.Args()
+
+	if len(args) < 2 {
+
+		_, err := ctx.EffectiveMessage.Reply(b, "Please provide a time limit, e.g., /open 300", nil)
+
+		return err
+
+	}
+
+	clockLimit := args[1]
+
+	increment := "0"
+
+	if len(args) > 2 {
+
+		increment = args[2]
+
+	}
+
+	openChallenge(b, ctx, clockLimit, increment, "Open Challenge Duel", false)
+
+	return nil
+
+}
+
+// start introduces the bot.
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+
+	const startMessage = `
+Hey\! I’m *Tewodros* ♟️🤖
+
+I’m the official *ETHCHESS* club bot 🏛️  
+Right now I’m still warming up, so I can’t chat naturally yet — but I *can* help you start games and throw down some chess battles 💥
+
+*Game commands:* 🎮
+
+/blitz      \- blitz game ⚡  
+/blitzr     \- rated blitz game ⚡  
+
+/bullet     \- bullet game   
+/bulletr    \- rated bullet game   
+
+/rapid      \- rapid game   
+/rapidr     \- rated rapid game   
+
+/open x y   \- custom time control ⏱️  
+\(x \= seconds, y \= increment\)
+
+Rated games affect rating   
+Unrated games are just for fun 😄  
+
+More features are coming soon — stay sharp 
+`
+	_, err := ctx.EffectiveMessage.Reply(b, startMessage, &gotgbot.SendMessageOpts{
+		ParseMode: "MarkdownV2",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send start message: %w", err)
 	}
 	return nil
 }
